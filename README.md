b5e0470420f8601cabfe75c25daba4b0a9a75f65
Aarron Lee <aclee91@gmail.com>
2020-09-05 21:28:45 -0400

    app build

---
+++ README.md
@@ -0,0 +1,3 @@
+### Habit Tracker
+
+compiled code for [simple habit tracker](https://www.github.com/aarron-lee/simple-habit-tracker)
---
+++ precache-manifest.ed83e55e16c060a0844370ecb3e0af9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb9580eb0c69e0724976d07567de3d4",
+    "url": "https://aarronlee.com/habit-tracker/index.html"
+  },
+  {
+    "revision": "c16f3ae560e70c2ea0df",
+    "url": "https://aarronlee.com/habit-tracker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7ac41c98185a9fa0185c",
+    "url": "https://aarronlee.com/habit-tracker/static/js/2.01f09f51.chunk.js"
+  },
+  {
+    "revision": "20ddbd5643449080fa85d452d1b6d6bd",
+    "url": "https://aarronlee.com/habit-tracker/static/js/2.01f09f51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c16f3ae560e70c2ea0df",
+    "url": "https://aarronlee.com/habit-tracker/static/js/main.75d68fda.chunk.js"
+  },
+  {
+    "revision": "966facfe7e62153e3b3b",
+    "url": "https://aarronlee.com/habit-tracker/static/js/runtime-main.05f86780.js"
+  }
+]);
